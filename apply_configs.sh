0eae409339bd4a3159fc2304502ea331d60eed24
Aditya Dhawale <adityadhawale6@gmail.com>
2020-07-20 22:01:29 -0700

    copies the spotify status

--- apply_configs.sh
+++ apply_configs.sh
@@ -6,6 +6,6 @@ mkdir ~/.config/i3/background && cp background/real_madrid.jpg ~/.config/i3/back
 cp termite/config ~/.config/termite/
 
 cp polybar/*.ini ~/.config/polybar/
-mkdir ~/.config/polybar/scripts && cp polybar/scripts/menu ~/.config/polybar/scripts/
+mkdir ~/.config/polybar/scripts && cp polybar/scripts/menu ~/.config/polybar/scripts/ && cp polybar/spotify_status.py ~/.config/polybar/spotify_status.py
 
 cp rofi/*.rasi ~/.config/rofi/
