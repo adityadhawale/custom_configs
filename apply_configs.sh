eba09e2f3077adc28b0ed0225c421785c7457dd8
Aditya Dhawale <aditya.dhawale@shield.ai>
2020-07-24 19:57:42 -0700

    adds a custom lockscreen

--- apply_configs.sh
+++ apply_configs.sh
@@ -2,6 +2,7 @@
 
 ######## copy configs to their default locations
 mkdir ~/.config/i3/background && cp background/real_madrid.jpg ~/.config/i3/background/
+cp i3/locker ~/.config/i3/ && cp i3/lock.png ~/.config/i3/ cp i3/config ~/.config/i3/
 
 cp termite/config ~/.config/termite/
 
@@ -9,3 +10,5 @@ cp polybar/*.ini ~/.config/polybar/
 mkdir ~/.config/polybar/scripts && cp polybar/scripts/menu ~/.config/polybar/scripts/ && cp polybar/spotify_status.py ~/.config/polybar/spotify_status.py
 
 cp rofi/*.rasi ~/.config/rofi/
+
+cp compton/compton.conf ~/.config/compton.conf
